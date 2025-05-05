4aabdd17b47360b9095eb43bddf6730df977eab9
halenurgurel <halenur.gurel@hotmail.com>
2025-05-05 17:17:52 +0300

    Add project files.

---
+++ neden-patika/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NedenPatika
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Halenur Gürel");
+            Console.WriteLine("Yazılıma olan ilgim ve Patika'nın sektördeki öncülüğü benim bu kursa katılmamdaki en büyük sebebtir. :)");
+        }
+
+    }
+}
